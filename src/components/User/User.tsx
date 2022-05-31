ade623b0be5f495c3101f0112531aa3abd1a9c41
Alexey Akulich <loki1987by@yandex.ru>
2022-05-31 09:29:07 +0300

    fix: buggfix

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,19 +1,44 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { UserPlaces } from "../../utils/types";
+import left from "../../assets/triangular-left.svg";
+import right from "../../assets/triangular-right.svg";
 import "./User.css";
 
 function User(): React.ReactElement {
   const store = useSelector((state: RootState) => state);
   const usersState = store.users;
   const postsState = store.posts;
+  const history = useHistory();
 
-  const id = +window.location.pathname.replace(/\D/gi, "");
-  const user = usersState.find((item) => item.id === id);
-  const posts = postsState.filter((post) => post.userId === id);
-  const postsPreview = posts.slice(0, 3);
+  let id = +window.location.pathname.replace(/\D/gi, "");
+  const currentUser = usersState.find((item) => item.id === id);
+  const currentUserPosts = postsState.filter((post) => post.userId === id);
+  const currentUserPostsPreview = currentUserPosts.slice(0, 3);
+  const [user, changeUser] = React.useState(currentUser);
+  const [postsPreview, changePostsPreview] = React.useState(
+    currentUserPostsPreview
+  );
+
+  const routeChange = (arg: boolean) => {
+    let newId = NaN;
+
+    if (arg) {
+      newId = id + 1;
+    } else {
+      newId = id - 1;
+    }
+    const path = `user/${newId}`;
+    history.replace(path);
+    id = newId;
+    const user = usersState.find((item) => item.id === newId);
+    changeUser(user);
+    const posts = postsState.filter((post) => post.userId === newId);
+    const postsPreview = posts.slice(0, 3);
+    changePostsPreview(postsPreview);
+  };
 
   return (
     <section className="User">
@@ -21,7 +46,22 @@ function User(): React.ReactElement {
         <div></div>
         <h2 className="User__data-username">{user?.username}</h2>
         <div></div>
-        <div></div>
+        <div
+          style={id > 1 ? { display: "flex", justifyContent: "center" } : {}}
+        >
+          {id > 1 ? (
+            <img
+              alt="Предыдущий пользователь"
+              onClick={() => {
+                routeChange(false);
+              }}
+              src={left}
+              className="User__data-button"
+            />
+          ) : (
+            ""
+          )}
+        </div>
         <div className="User__data-description">
           <div>
             <h3>{user?.name}</h3>
@@ -36,7 +76,28 @@ function User(): React.ReactElement {
             <h3>{user?.website}</h3>
           </div>
         </div>
-        <div></div>
+        <div
+          style={
+            id < usersState.length
+              ? { display: "flex", justifyContent: "center" }
+              : {}
+          }
+        >
+          {id < usersState.length ? (
+            <Link to={`/user/${id + 1}`}>
+              <img
+                alt="Следующий пользователь"
+                onClick={() => {
+                  routeChange(true);
+                }}
+                src={right}
+                className="User__data-button"
+              />
+            </Link>
+          ) : (
+            ""
+          )}
+        </div>
         <div></div>
         <div className="User__data-company-name">
           <h3>Место работы:</h3>
@@ -49,17 +110,23 @@ function User(): React.ReactElement {
         <h2>Посты:</h2>
         <div className="User__data-posts-container">
           {postsPreview.map((post, index) => (
-            <div className="User__data-post" key={index}>
-              <h2
-                className="User__data-post-title post-preview"
-                title={`${post.title}`}
-              >
-                {post.title}
-              </h2>
-              <h4 className="post-preview" title={`${post.body}`}>
-                {post.body}
-              </h4>
-            </div>
+            <Link
+              style={{ width: "32%", textDecoration: "none" }}
+              key={index}
+              to={`/post/${post.id}`}
+            >
+              <div className="User__data-post">
+                <h2
+                  className="User__data-post-title post-preview"
+                  title={`${post.title}`}
+                >
+                  {post.title}
+                </h2>
+                <h4 className="post-preview" title={`${post.body}`}>
+                  {post.body}
+                </h4>
+              </div>
+            </Link>
           ))}
         </div>
         <Link to={`/posts/${id}`} className="User__data-posts-button">
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,23 +6,47 @@ import { UsersProps } from "../../utils/types";
 import "./Users.css";
 
 function Users(props: UsersProps): React.ReactElement {
+  const [cardsStyle, setCardsStyle] = React.useState(true)
+  const [styleSwitcherText, setStyleSwitcherText] = React.useState('Отобразить списком')
   const store = useSelector((state: RootState) => state);
   const usersState = store.users;
   React.useEffect(() => {
     props.setUsersPage(true);
   }, [props]);
 
+  function changeUlType() {
+    const newState = !cardsStyle
+    setCardsStyle(newState)
+    if (styleSwitcherText === 'Отобразить списком') {
+      setStyleSwitcherText('Отобразить плиткой')
+    } else {
+      setStyleSwitcherText('Отобразить списком')
+    }
+  }
+
   return (
     <section className="Users">
+      <div className="Users__header">
       <h2>Пользователи</h2>
-      <ul className="Users__list">
-        {usersState.map((user, index) => (
+      <button onClick={changeUlType}>{styleSwitcherText}</button>
+      </div>
+      <ul className={`Users__list ${cardsStyle && "Users__card-list"}`}>
+        {usersState.map((user, index) => {
+          return !cardsStyle ?
           <li key={index}>
             <Link to={`/user/${user.id}`} className="Users__list-item">
-              {user.name as string}
+              {user.name as string} @{user.username as string}
+            </Link>
+          </li> :
+          <li key={index} className="Users__list-card">
+            <Link to={`/user/${user.id}`} className="Users__list-item">
+              <div>
+              <h3>{user.name as string}</h3>
+              <h3>@{user.username as string}</h3>
+              </div>
             </Link>
           </li>
-        ))}
+})}
       </ul>
     </section>
   );
